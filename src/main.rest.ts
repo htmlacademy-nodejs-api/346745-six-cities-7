b28bed2cc3d518e8d6cc0b59b0680c65a81adb20
alexandershubin <shubin4433@gmail.com>
2024-05-13 17:29:58 +0200

    module4-task1 - connected to mongoDB

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -4,12 +4,14 @@ import { LoggerInterface, PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
 import { ConfigInterface, RestConfig, TRestSchema } from './shared/libs/config/index.js';
 import { Component } from './shared/types/index.js';
+import {DatabaseClient, MongoDatabaseClient} from './shared/libs/database-client/index.js';
 
 async function bootstrap () {
   const container = new Container();
   container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<LoggerInterface>(Component.Logger).to(PinoLogger).inSingletonScope();
   container.bind<ConfigInterface<TRestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
+  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
 
   const application = container.get<RestApplication>(Component.RestApplication);
 
--- src/shared/helpers/index.ts
+++ src/shared/helpers/index.ts
@@ -8,3 +8,4 @@ export {
 } from './common.js';
 
 export { getCurrentModuleDirectoryPath } from './file-system.js';
+export { getMongoURI } from './database.js';
