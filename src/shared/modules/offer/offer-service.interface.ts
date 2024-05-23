75a5bbe5388bbb27c3fdf31b01a3c5a9b7145591
alexandershubin <shubin4433@gmail.com>
2024-05-23 23:26:51 +0200

    module5-task1 - add spec

--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -2,9 +2,17 @@ import { DocumentType } from '@typegoose/typegoose';
 
 import { CreateOfferDto } from './dto/create-offer.dto.js';
 import { OfferEntity } from './offer.entity.js';
+import { UpdateOfferDto } from './dto/update-offer.dto.js';
 
 export interface OfferService {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  find(): Promise<DocumentType<OfferEntity>[]>;
+  deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null>;
+  incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  findNew(count: number): Promise<DocumentType<OfferEntity>[]>;
+  findDiscussed(count: number): Promise<DocumentType<OfferEntity>[]>;
+  exists(documentId: string): Promise<boolean>;
 }
 
